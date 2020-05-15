fixed accuracy calculation bugs

main.py
@@ -115,8 +115,8 @@ def train_model(model, dataloaders, criterion, optimizer, device, num_epochs=25)
                 running_loss += loss.item() * inputs.size(0)
                 running_corrects += torch.sum(preds == labels.data)
 
-            epoch_loss = running_loss / len(dataloaders[phase].dataset)
-            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+            epoch_loss = running_loss / train_num if phase == 'train' else running_loss / val_num
+            epoch_acc = running_corrects.double() / train_num if phase == 'train' else running_corrects.double() / val_num
 
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
 
@@ -189,9 +189,9 @@ def main():
                         help='training data path')
     parser.add_argument('--test-img-path', type=str, default='./data/test',
                         help='test data path')
-    parser.add_argument('--train-txt-path', type=str, default='./data/train_split_v3.txt',
+    parser.add_argument('--train-txt-path', type=str, default='./train_split_v3.txt',
                         help='train txt path')
-    parser.add_argument('--test-txt-path', type=str, default='./data/test_split_v3.txt',
+    parser.add_argument('--test-txt-path', type=str, default='./test_split_v3.txt',
                         help='test txt path')
     parser.add_argument('--model-save-path', type=str, default='./baseline.pth',
                         help='model save path')
@@ -257,10 +257,14 @@ def main():
     train_index.extend(train_covid_index)
     train_index.extend(train_normal_index)
     train_index.extend(train_pneumonia_index)
-    print('train_index', len(train_index))
+    global train_num
+    train_num = len(train_index)
+    print('train_num', train_num)
 
     val_index = np.setdiff1d(range(len(train_val_dataset)), train_index)
-    print('val_index', len(val_index))
+    global val_num
+    val_num = len(val_index)
+    print('val_num', val_num)
 
     train_loader = torch.utils.data.DataLoader(train_val_dataset, batch_size=args.batch_size,
                                                sampler=SubsetRandomSampler(train_index))